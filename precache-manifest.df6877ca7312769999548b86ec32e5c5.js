dd484a015a1a3b6f6680fa603fe927c42a8ee071
SanthoshNC <SanthoshNC@users.noreply.github.com>
2024-11-06 01:20:26 +0000

    deploy: 0be05a13333cf90b3b711a06a625ffe5535ded82

---
+++ precache-manifest.df6877ca7312769999548b86ec32e5c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0acdf636c11503dad7c5502b61dc9919",
+    "url": "/inferix-demo/index.html"
+  },
+  {
+    "revision": "19923d005eca9247f9ee",
+    "url": "/inferix-demo/static/css/main.e32d0714.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/inferix-demo/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/inferix-demo/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "19923d005eca9247f9ee",
+    "url": "/inferix-demo/static/js/main.ef10d6ca.chunk.js"
+  },
+  {
+    "revision": "2a1404b5371178ed6bbb",
+    "url": "/inferix-demo/static/js/runtime~main.13fbfd46.js"
+  }
+]);
